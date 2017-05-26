de1ff64a152f5a7bcabfd3e176dee171d1ec032d
Carlos Serrada <cserradag96@gmail.com>
2017-05-26 01:23:42 -0400

    Intento de template para iniciar el desarrollo xD

--- static/js/shared/start-please-wait.js
+++ static/js/shared/start-please-wait.js
@@ -1,5 +1,4 @@
 var loading_screen = pleaseWait({
-    logo: "dist/img/inea-tugs-logo.png",
-    backgroundColor: '#011924',
+    backgroundColor: '#2196F3',
     loadingHtml: '<div class="sk-three-bounce"><div class="sk-child sk-bounce1"></div><div class="sk-child sk-bounce2"></div><div class="sk-child sk-bounce3"></div></div>'
 });
