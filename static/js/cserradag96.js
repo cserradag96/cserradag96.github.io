413903b88005a7010ed42e6d51c6c96ef4b54e10
Carlos Serrada <cserradag96@gmail.com>
2017-05-27 00:52:40 -0400

    Prueba de lectura

--- static/js/cserradag96.js
+++ static/js/cserradag96.js
@@ -29,5 +29,7 @@ cserradag96.config(function($stateProvider, $urlRouterProvider) {
 # INITIAL GLOBAL SETTINGS
 #########################################################################################################################################################################################################################*/
 cserradag96.run(function ($rootScope) {
-    $rootScope.baseURL = "http://cserradag96.github.io/";        /* Server domain */
+    $rootScope.baseURL  = "http://cserradag96.github.io/";        /* Server domain */
+    $rootScope.apiURL   = $rootScope.baseURL + "static/apis/";    /* Server domain */
+    $rootScope.lenguage = "es";                                   /* Default lenguage */
 });
