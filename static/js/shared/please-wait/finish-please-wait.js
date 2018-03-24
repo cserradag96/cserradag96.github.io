08bffdac1d2b93addc822420fde256469cfbecf7
cserradag96 <cserradag96@gmail.com>
2018-03-24 18:45:41 -0400

    Finished optimizations

--- static/js/shared/please-wait/finish-please-wait.js
+++
@@ -1,1 +0,0 @@
-setTimeout(function(){loading_screen.finish(true);}, 2500);
--- static/js/shared/please-wait/start-please-wait.js
+++
@@ -1,5 +0,0 @@
-var loading_screen = pleaseWait({
-    logo: "static/img/logo.png",
-    backgroundColor: '#2196F3',
-    loadingHtml: '<div class="sk-three-bounce"><div class="sk-child sk-bounce1"></div><div class="sk-child sk-bounce2"></div><div class="sk-child sk-bounce3"></div></div>'
-});
